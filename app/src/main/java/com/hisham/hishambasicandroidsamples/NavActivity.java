d4cdf83b5481295a3a2e39c6af713faf52ddc287
Hisham <hisham.dbd@gmail.com>
2018-10-14 11:32:41 +0530

    Cursor loader example

--- app/src/main/java/com/hisham/hishambasicandroidsamples/NavActivity.java
+++ app/src/main/java/com/hisham/hishambasicandroidsamples/NavActivity.java
@@ -1,23 +1,34 @@
 package com.hisham.hishambasicandroidsamples;
 
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.lifecycle.Observer;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.SparseArray;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.hisham.hishambasicandroidsamples.handler.HandlerActivity;
+import com.hisham.hishambasicandroidsamples.loaderz.CursorLoaderActivity;
 import com.hisham.hishambasicandroidsamples.service_bound.BoundedAIDLActivity;
 import com.hisham.hishambasicandroidsamples.service_bound.BoundedBinderActivity;
 import com.hisham.hishambasicandroidsamples.service_bound.BoundedMessengerActivity;
 
 import java.net.Inet6Address;
 import java.net.InetAddress;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Random;
 
 public class NavActivity extends AppCompatActivity {
 
@@ -26,12 +37,29 @@ public class NavActivity extends AppCompatActivity {
             BoundedBinderActivity.class,
             BoundedMessengerActivity.class,
             BoundedAIDLActivity.class,
+            CursorLoaderActivity.class,
     };
+
+          MutableLiveData<Object> data = new MutableLiveData<>();
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        data.postValue("tst: " + new Random().nextInt());
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nav);
+          data.observe(this, new Observer<Object>() {
+              @Override
+              public void onChanged(@Nullable Object o) {
+                  if (o != null) {
+                      System.out.print(o.toString());
+                  }
+              }
+          });
         RecyclerView mRecyclerView = findViewById(R.id.rV);
         // use this setting to improve performance if you know that changes
         // in content do not change the layout size of the RecyclerView
